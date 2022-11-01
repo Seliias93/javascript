65ca7370f5ae7dcbce6be9817e4954909b8f03ee
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-11-01 11:38:10 -0300

    .

--- Ex02/O-que-e-o-Dom.js
+++ Ex02/O-que-e-o-Dom.js
@@ -39,15 +39,20 @@ titulo.classList; // retorna as classes;
 titulo.id; // retorna o id;
 titulo.offsetHeight; // retorna a altura do elemento;
 
-titulo.addEventListener('click', callback);
+titulo.addEventListener(click, callback);
 // ativa a função callback ao click no titulo
 
 EXERCÍCIO
 // Retorne o url da página atual utilizando o objeto window
-
+const hrefPagina = window.location.href
+console.log(hrefPagina);
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
-
+const elementoAtivo = document.querySelector('ativo');
 // Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
 
 // Retorne a largura da janela 
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
