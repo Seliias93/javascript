a9b4636e800b34fd303d89c8c9dcc05614f7139f
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-11-24 15:52:56 -0300

    .

--- Ex02/Array.js
+++ Ex02/Array.js
@@ -171,17 +171,36 @@ linguagens.slice(1, 4); // ['css', 'js', 'php']
 const cloneLinguagens = linguagens.slice();
 
 //EXERCÍCIOS
+
 const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
 // Remova o primeiro valor de comidas e coloque em uma variável
 // Remova o último valor de comidas e coloque em uma variável
 // Adicione 'Arroz' ao final da array
 // Adicione 'Peixe' e 'Batata' ao início da array
 
+const primeiroValor = comida.shift();
+const ultimoValor   = comida.pop();
+
+comida.push('Arroz');
+comida.unshift('Peixe', 'batata');
+
+console.log(primeiroValor)
+console.log(ultimoValor)
+console.log(comidas)
+
+
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
 // Inverta a ordem dos estudantes
 // Verifique se Joana faz parte dos estudantes
 // Verifique se Juliana faz parte dos estudantes
+
+estudantes.short();    //Orde Cronológia Alfabetica
+estudantes.reverse();  //Ordem Cronológia inversa Alfabetica
+
+console.log(estudantes);
+console.log(estudantes.includes('Joana'))
+console.log(estudantes.includes('Juliana'))
 
 let html = `<section>
               <div>Sobre</div>
@@ -191,6 +210,16 @@ let html = `<section>
 // Substitua section por ul e div com li,
 // utilizando split e join
 
-const carros9  = ['Ford', 'Fiat', 'VW', 'Honda'];
+html =html.split('section').join('ul').split('div').join('li');
+console.log(html)
+
+const carros9    = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+
+const carroCopia = carros.slice();
+
+carros.pop();
+
+console.log(carros);
+console.log(carrosCopia);
