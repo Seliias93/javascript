624cefa08f52bd50136e5462ba2f0cdbf81c20c6
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-12-02 11:58:53 -0300

    .

--- Ex04/Animais01/JavaScript/initmodal.js
+++ Ex04/Animais01/JavaScript/initmodal.js
@@ -1,15 +1,28 @@
+
 const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-const botaoFechar = document.querySelector('[data-modal="fecha"]');
+const botaoFecha = document.querySelector('[data-modal="fecha"]');
 const containerModal = document.querySelector('[data-modal="container"]');
 
+if(botaoAbrir && botaoFecha && containerModal){
 function abrirModal(){
     event.preventDefault();
     containerModal.classList.add('ativo');
     console.log(event)
 }
-function fechaModal(){
 
+function fechaModal(){
+    event.preventDefault();
+    containerModal.classList.remove('ativo');
+}
+
+function cliqueForaModal(event){
+    if(event.target === this){
+    fechaModal(event);
+    }
 }
 
 botaoAbrir.addEventListener('click', abrirModal);
-botaoFechar.addEventListener('click', fechaModal);
+botaoFecha.addEventListener('click', fechaModal);
+containerModal.addEventListener('click', cliqueForaModal);
+}
+
