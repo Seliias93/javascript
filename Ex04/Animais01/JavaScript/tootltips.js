761c53d66425c452f48d67e148b40eee9ae8f504
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-12-03 15:34:59 -0300

    .

--- Ex04/Animais01/JavaScript/tootltips.js
+++ Ex04/Animais01/JavaScript/tootltips.js
@@ -7,20 +7,30 @@ tooltips.forEach((item) => {
 
 function onMouseOver(event){
     const tooltipBox = criarTooltipbox(this);
-    tooltipBox.style.top = event.pageY + 'px';
-    tooltipBox.style.left = event.pageX + 'px';
-
-
+    
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove);
     onMouseLeave.tooltipBox = tooltipBox;
-    this.addEventListener('mouseleave' , onMouseLeave);
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+    
 }
 
 const onMouseLeave = {
-    tooltipBox: '',
     handleEvent() {
         this.tooltipBox.remove();
+        this.element.removeEventListener('mouseleave', onMouseLeave);
+        this.element.removeEventListener('mousemove', onMouseMove);
     }
 }
+
+const onMouseMove = {
+    handleEvent(event){
+        this.tooltipBox.style.top = event.pageY + 20 + 'px';
+        this.tooltipBox.style.left = event.pageX + 20 +'px';
+    }
+}
+
 
 function criarTooltipbox(element){
     const tooltipBox = document.createElement('div');
