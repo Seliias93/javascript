75098db937183db58155f9043891c341f88c4c91
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-12-03 14:45:55 -0300

    .

--- Ex04/Animais01/JavaScript/tootltips.js
+++ Ex04/Animais01/JavaScript/tootltips.js
@@ -1,1 +1,20 @@
-7.-''
+
+const tooltips =document.querySelectorAll('[data-tooltip]');
+
+tooltips.forEach((item) => {
+    item.addEventListener('mouseover', onMouseOver)
+})
+
+function onMouseOver(event){
+    const tooltipBox = criarTooltipbox(this);
+    console.log(event)
+}
+
+function criarTooltipbox(element){
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+}
