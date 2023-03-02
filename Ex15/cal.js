692f8fc8e30ce6184b6f462ede1e8da947f9f560
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-03-02 11:52:14 -0300

    .

---
+++ Ex15/cal.js
@@ -0,0 +1,8 @@
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite outro numero');
+
+num1 = parseFloat (num1);
+num2 = parseFloat (num2);
+
+const resultador = num1 + num2 ;
+alert(`o resultador da sua conta foi : ${resultador}`);
