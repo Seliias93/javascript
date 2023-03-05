ce270ac122a1eac3c4a68dc07f2d0d6b1f3e452a
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-03-05 07:51:35 -0300

    .

--- Ex15/index.js
+++ Ex15/index.js
@@ -2,9 +2,7 @@ let  varA = 'A'; //B
 let  varB = 'B'; //c
 let  varC = 'c'; //A
 
-const  varAtemp = varA;
-varA = varB;
-varB = varC;
-varC = varAtemp;
+
+[varA , varB, varC] = [varB, varC, varA]
 
 console.log(varA, varB, varC)
