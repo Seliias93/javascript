7336e7b9b5322f322d7ba2b7c1b65a5056880d26
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-03-14 11:39:09 -0300

    .

--- aula25/index.js
+++ aula25/index.js
@@ -7,6 +7,7 @@ if (numero <= 10) { // Verdadeira
   console.log('O número é menor ou igual a 10.');
 }
 
+
 if (numero >= 0 && numero <= 5) {
   console.log('O número está entre 0 e 5.');
 } else if (numero >= 6 && numero <= 8) {
@@ -18,3 +19,4 @@ if (numero >= 0 && numero <= 5) {
 }
 
 console.log('...Aqui vai o resto do código.');
+  
