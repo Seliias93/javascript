67c684b2b5158216c3c6612b01952e8d2a50c3c6
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-03-19 12:51:53 -0300

    .

--- aula34/index.js
+++ aula34/index.js
@@ -1,3 +1,4 @@
 const frutas = ['Maçã', 'Pêra', 'Uva'];
 const novasFrutas = frutas.map(el => el.toLowerCase());
+
 console.log(novasFrutas); // [ 'maçã', 'pêra', 'uva' ]
--- aula35/assets/js/main.js
+++ aula35/assets/js/main.js
@@ -8,7 +8,7 @@ const elementos = [
 const container = document.querySelector('.container');
 const div = document.createElement('div');
 
-for (let i = 0; i < elementos.length; i++) {
+for (let i = 0; i <elementos.length; i ++){
   let { tag, texto } = elementos[i];
   let tagCriada = document.createElement(tag);
   let textoCriado = document.createTextNode(texto);
@@ -16,5 +16,6 @@ for (let i = 0; i < elementos.length; i++) {
   tagCriada.appendChild(textoCriado);
   div.appendChild(tagCriada);
 }
+
 
 container.appendChild(div);
